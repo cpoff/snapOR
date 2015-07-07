2cc239912cefdda4f4d8371d716af39a5e170758
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-07 10:58:54 -0700

    created basic marker with infowindow

--- scripts/map.js
+++ scripts/map.js
@@ -1,19 +1,31 @@
 function initialize(){
 	var mapCanvas = document.getElementById('map_canvas'); //map will be placed in the div with id="map_canvas"
+	var Bend = new google.maps.LatLng(44.058173, -121.31531);
+	//to find lat/lng of locations: http://www.latlong.net/
+
 	var mapOptions = {
-		center : new google.maps.LatLng(44, -121), //this centers the map on Bend, OR
+		center : Bend, //this centers the map on Bend, OR
 		zoom : 6, //range 0 - 22, 22 is the closest zoomed in, 6 will show all of oregon
 		mapTypeId : google.maps.MapTypeId.TERRAIN //additional options:ROADMAP, SATELLITE, HYBRID
 	}
 	var map = new google.maps.Map(mapCanvas, mapOptions);
 
-	var myMarker = new google.maps.LatLng(44, -121);
-	var marker = new google.maps.Marker({
-		position: myMarker,
-		// map: map,
-		title: 'Bend'
+	//sample code to create new markers with info windows
+	var bend_content = "<div><h1>Bend, OR</h1></div>";
+	var infowindow = new google.maps.InfoWindow({
+		content: bend_content
 	});
-	marker.setMap(map);
+	var Bend_marker = new google.maps.Marker({
+		position: Bend,
+		map: map,
+		title: 'Bend',
+		draggable: true,
+		animation: google.maps.Animation.DROP,
+	});
+	// marker.setMap(map); additional method to set the marker on map
+	google.maps.event.addDomListener(Bend_marker, 'click', function(){
+		infowindow.open(map, Bend_marker);
+	})
 }
 
 google.maps.event.addDomListener(window, 'load', initialize); //loads the map on the page once the html is loaded
