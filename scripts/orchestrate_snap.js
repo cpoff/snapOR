4b787edeaf8c1bf971c259a5e6ac31ab30488223
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-07 17:53:50 -0700

    database put function will store data

--- scripts/orchestrate_snap.js
+++ scripts/orchestrate_snap.js
@@ -1,1 +1,20 @@
-var orchestrate = require('orchestrate');
+var config = require('./config.js');
+var db = require('orchestrate')(config.dbKey);
+
+//store email, name, home location, array of favorite parks
+
+var email;
+var name;
+var home_location;
+var favs = [];
+
+db.put('snap', 'key', {
+	'name': name,
+	'email': email,
+	'home location': home_location,
+	'favorite parks': favs
+})
+.then(function(result){
+	console.log('user created');
+})
+.fail(function(err){})
