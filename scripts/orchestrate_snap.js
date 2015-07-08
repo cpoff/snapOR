d31f21edcc720453af588fa2edfd7b05af09f032
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-08 09:41:10 -0700

    orchestrate database set up, need server side code to connect

--- scripts/orchestrate_snap.js
+++ scripts/orchestrate_snap.js
@@ -1,14 +1,17 @@
 var config = require('./config.js');
 var db = require('orchestrate')(config.dbKey);
+var uuid = require('uuid');
 
 //store email, name, home location, array of favorite parks
 
-var email;
-var name;
-var home_location;
+var email = document.getElementById();
+var name = document.getElementById();
+var home_location = document.getElementById();
 var favs = [];
+var user_key = uuid.v4();
 
-db.put('snap', 'key', {
+//set a value and allow the serer to generate a key
+db.post('snap', {
 	'name': name,
 	'email': email,
 	'home location': home_location,
@@ -18,3 +21,30 @@ db.put('snap', 'key', {
 	console.log('user created');
 })
 .fail(function(err){})
+
+
+//set the values for a new user
+db.put('snap', user_key, {
+	'name': name,
+	'email': email,
+	'home location': home_location,
+	'favorite parks': favs
+})
+.then(function(result){
+	console.log('user created');
+})
+.fail(function(err){})
+
+//update the values
+db.merge('snap', user_key, {
+	'name': name,
+	'email': email,
+	'home location': home_location,
+	'favorite parks': favs
+})
+
+//remove the user data
+db.remove('snap', user_key, true)
+.then(function(result){
+	console.log("user deleted");
+}).fail({})
