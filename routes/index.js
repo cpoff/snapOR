77507b0c27527c4109b73fa2b45cd34a57e6a332
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-08 16:33:49 -0700

    refactored registeration modal, created post request

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config.js');
+var db = require('orchestrate')(config.dbKey);
+var uuid = require('uuid');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,5 +13,25 @@ module.exports = router;
 
 
 //post user registration data
+router.post('/register', function(req, res){
+	// var email = document.getElementById();
+	// var name = document.getElementById();
+	// var home_location = document.getElementById();
+	var email = req.body.email;
+	var password = req.body.password;
+	var password_confirm = reqest.body.password_confirm;
+	var user_key = uuid.v4();
+
+	if (password === password_confirm){
+		db.put('snap', user_key, {
+			'email': email,
+			'password': password
+		})
+		.then(function(result){
+			console.log('user created');
+		})
+		.fail(function(err){})
+	}
+});
 //put user data
 //delete user
