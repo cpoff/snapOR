361a1a2e9fef8401637172094e582e03a20e63b1
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-10 13:48:54 -0700

    removed unnecessary div where there's a class or id on jade

--- public/javascripts/lat-long.js
+++ public/javascripts/lat-long.js
@@ -50,58 +50,44 @@ function initialize(){
 function go() {
 	$.ajax(url, {data: data})
 	.then(function(data, status, xhr) {
-	// console.log(parkArray + "2");
 		parkData = data;
 		latLong();
-	// console.log(parkArray + "3");
 	}).then(function(){
 		initialize();
 		google.maps.event.addDomListener(window, 'load', initialize);
 	}).then(function(){
 		var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
- 
-    // an array that will be populated with substring matches
-    matches = [];
- 
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
- 
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        matches.push(str);
-      }
-    });
- 
-    cb(matches);
-  };
-};
- 
-// var parkNameArray = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-//   'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-//   'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-//   'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-//   'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-//   'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-//   'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-//   'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-//   'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-// ];
-
-$(function(){
-  $('#the-basics .typeahead').typeahead({
-    hint: true,
-    highlight: true,
-    minLength: 1
-  },
-  {
-    name: 'parkNameArray',
-    source: substringMatcher(parkNameArray)
-  });
-});
+		  return function findMatches(q, cb) {
+		    var matches, substringRegex;
+		 
+		    // an array that will be populated with substring matches
+		    matches = [];
+		 
+		    // regex used to determine if a string contains the substring `q`
+		    substrRegex = new RegExp(q, 'i');
+		 
+		    // iterate through the pool of strings and for any string that
+		    // contains the substring `q`, add it to the `matches` array
+		    $.each(strs, function(i, str) {
+		      if (substrRegex.test(str)) {
+		        matches.push(str);
+		      }
+		    });
+		 
+		    cb(matches);
+		  };
+		};
+		$(function(){
+		  $('#the-basics .typeahead').typeahead({
+		    hint: true,
+		    highlight: true,
+		    minLength: 1
+		  },
+		  {
+		    name: 'parkNameArray',
+		    source: substringMatcher(parkNameArray)
+		  });
+		});
 	});
 };
 
