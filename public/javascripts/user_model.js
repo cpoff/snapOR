f4a6f822eeb1c428c47c2ed6170108bae0e767bf
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-15 11:31:42 -0700

    in the middle of creating different methods in UserView

--- public/javascripts/user_model.js
+++ public/javascripts/user_model.js
@@ -24,33 +24,54 @@ var UserModel = Backbone.Model.extend({
 var UserView = Backbone.View.extend({
 	url: '/user',
 	render: function() {
-		//if the user doesn't have property values for name and home location, render this template (to collect this data)
-		var update_user_template = _.template('<h2>Update</h2><label>Name: </label><input type="text" id="nameInput" placeholder="enter name" value=""</input><br /><label>Email: </label><input type="text" id="emailInput" placeholder="enter email" value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder="enter home location" value=""</input><br /><label>Password: </label><input type="text" id="homeInput" placeholder="enter home location" value=""</input><br /><button type="submit" id="homeUpdate">Update Info</button>');
-		var new_user_tempate = _.tempate('<h2>Welcome</h2><p>We have a couple more questions for you so we can make your experience with snapOR more personal.<p><label>Name: </label><input type="text" id="nameInput" placeholder="enter name" value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder="enter home location" value=""</input><br /><button type="submit" id="homeUpdate">Save Info</button>');
-		var user_template;
-		//else render a different template
-			//var nameVal = this.model.get("name");
-			var nameInput = '<label>Name: </label><input type="text" id="nameInput" placeholder="enter name" value=""</input>';
-			var nameBtn = '<button type="submit" id="nameUpdate">Update Info</button>';
-			//var emailVal = this.model.get("email");
-			var emailInput = '<label>Email: </label><input type="text" id="emailInput" placeholder="enter email" value=""</input>';
-			var emailBtn = '<button type="submit" id="emailUpdate">Update Info</button>';
-			//var homeVal = this.model.get("home");
-			var homeInput = '<label>Home Location: </label><input type="text" id="homeInput" placeholder="enter home location" value=""</input>';
-			var homeBtn = '<button type="submit" id="homeUpdate">Update Info</button>';
-			this.$el.html(nameInput + nameBtn + '<br />' + emailInput + emailBtn + '<br />' + homeInput + homeBtn);
+		var nameVal = this.model.get("name");
+		var emailVal = this.model.get("email");
+		var homeVal = this.model.get("home");
+		//this template will appear if the user clicks on the update btn
+		var new_user_tempate = _.tempate('<h2>Welcome</h2><p>We have a couple more questions for you so we can make your experience with snapOR more personal.<p><label>Name: </label><input type="text" id="nameInput" placeholder="Who are you?" value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder="Where do you live?" value=""</input><br /><button type="submit" id="save">Save Info</button>');
+		//this template will display if 
+		var user_template = _.template('<h1>Welcome {{nameVal}}<button type="submit" id="updatebtn">Update info</button><button type="submit" id="logout">Logout</button>');
+		//if the user doesn't have property values for name and home location, render the new_user_template
+		if(nameVal === '' && homeVal === ''){
+			this.$el.html(new_user_template());
+		} else{
+			this.$el.html(user_template());
+		}
 	}, // closes render again
-	replace: function() {
-			var str = this.$el.find("input").val();
-			this.model.replace(str);
+	update: function(){
+		var nameVal = this.model.get("name");
+		var emailVal = this.model.get("email");
+		var homeVal = this.model.get("home");
+		var update_user_template = _.template('<h2>Update</h2><label>Name: </label><input type="text" id="nameInput" placeholder={{nameVal}} value=""</input><br /><label>Email: </label><input type="text" id="emailInput" placeholder={{emailVal}} value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder={{homeVal}} value=""</input><br /><label>Password: </label><input type="text" id="password" placeholder="change password" value=""</input><br /><button type="submit" id="update">Update Info</button>');
+		this.$el.html(update_user_template);
+	},
+	save: function() {
+		//data before changes made
+		var nameVal = this.model.get("name");
+		var emailVal = this.model.get("email");
+		var homeVal = this.model.get("home");
+		//changes made on form
+		var nameChanged = this.$el.find("#nameInput");
+		var emailChanged = this.$el.find("#emailInput");
+		var homeChanged = this.$el.find("#homeInput");
+
+		if(nameVal !== nameChanged){
+			this.model.replace(nameVal);
+		}
+		if(emailVal !== emailChanged){
+			this.model.replace(emailVal);
+		}
+		if(homeVal !== homeChanged){
+			this.model.replace(homeVal);
+		}
 	},
 	initialize: function() {
 			this.model.on("change", this.render, this);
 	},
 	events: {
-			'click #nameUpdate': "replace",
-			'click #emailUpdate': "replace",
-			'click #homeUpdate': "replace"
+			'click #update': "update",
+			'click #logout': "logout",
+			'click #save': "save"
 	}, //closes events
 }); // closes userView
 
