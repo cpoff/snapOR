66f1bd99449d1f689e534d9dc91c581f96276ea8
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-15 14:38:00 -0700

    templates moved into the view

--- public/javascripts/user_model.js
+++ public/javascripts/user_model.js
@@ -1,6 +1,6 @@
 _.templateSettings = {
 	interpolate: /\{\{(.+?)\}\}/g
-}
+};
 
 //user page
 var UserModel = Backbone.Model.extend({
@@ -24,29 +24,38 @@ var UserModel = Backbone.Model.extend({
 
 var UserView = Backbone.View.extend({
 	url: '/user',
+	new_user_template :  _.template('<h2>Welcome</h2><p>We have a couple more questions for you so we can make your experience with snapOR more personal.<p><label>Name: </label><input type="text" id="nameInput" placeholder="Who are you?" value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder="Where do you live?" value=""</input><br /><button type="submit" id="save">Save Info</button>'),
+	user_template : _.template('<h1>Welcome {{nameVal}}<button type="submit" id="update">Update info</button><button type="submit" id="logout">Logout</button>'),
+	update_user_template : _.template('<h2>Update</h2><label>Name: </label><input type="text" id="nameInput" placeholder={{nameVal}} value=""</input><br /><label>Email: </label><input type="text" id="emailInput" placeholder={{emailVal}} value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder={{homeVal}} value=""</input><br /><label>Password: </label><input type="text" id="password" placeholder="change password" value=""</input><br /><button type="submit" id="save">Update Info</button>'),
+	// nameVal : this.model.get("name"),
+	// emailVal : this.model.get("email"),
+	// homeVal : this.model.get("home"),
 	render: function() {
 		var nameVal = this.model.get("name");
 		var emailVal = this.model.get("email");
 		var homeVal = this.model.get("home");
 		//this template will appear if the user clicks on the update btn
-		var new_user_tempate = _.tempate('<h2>Welcome</h2><p>We have a couple more questions for you so we can make your experience with snapOR more personal.<p><label>Name: </label><input type="text" id="nameInput" placeholder="Who are you?" value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder="Where do you live?" value=""</input><br /><button type="submit" id="save">Save Info</button>');
+		// var new_user_tempate = _.tempate('<h2>Welcome</h2><p>We have a couple more questions for you so we can make your experience with snapOR more personal.<p><label>Name: </label><input type="text" id="nameInput" placeholder="Who are you?" value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder="Where do you live?" value=""</input><br /><button type="submit" id="save">Save Info</button>');
 		//this template will display if 
-		var user_template = _.template('<h1>Welcome {{nameVal}}<button type="submit" id="update">Update info</button><button type="submit" id="logout">Logout</button>');
+		// var user_template = _.template('<h1>Welcome {{nameVal}}<button type="submit" id="update">Update info</button><button type="submit" id="logout">Logout</button>');
 		//if the user doesn't have property values for name and home location, render the new_user_template
-		if(nameVal === '' && homeVal === ''){
-			this.$el.html(
-				new_user_template());
-		} else{
-			this.$el.html(
-				user_template({nameVal : this.model.get("name")}));
-		}
+		this.$el.html(this.user_template(this.model.attributes));
+		this.input = this.$('.edit');
+		return this;
+		// if(nameVal === '' && homeVal === ''){
+		// 	this.$el.html(
+		// 		new_user_template());
+		// } else{
+		// 	this.$el.html(
+		// 		user_template({nameVal : this.model.get("name")}));
+		// }
 	}, // closes render again
 	update: function(){
 		var nameVal = this.model.get("name");
 		var emailVal = this.model.get("email");
 		var homeVal = this.model.get("home");
-		var update_user_template = _.template('<h2>Update</h2><label>Name: </label><input type="text" id="nameInput" placeholder={{nameVal}} value=""</input><br /><label>Email: </label><input type="text" id="emailInput" placeholder={{emailVal}} value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder={{homeVal}} value=""</input><br /><label>Password: </label><input type="text" id="password" placeholder="change password" value=""</input><br /><button type="submit" id="save">Update Info</button>');
-		this.$el.html(update_user_template);
+		// var update_user_template = _.template('<h2>Update</h2><label>Name: </label><input type="text" id="nameInput" placeholder={{nameVal}} value=""</input><br /><label>Email: </label><input type="text" id="emailInput" placeholder={{emailVal}} value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder={{homeVal}} value=""</input><br /><label>Password: </label><input type="text" id="password" placeholder="change password" value=""</input><br /><button type="submit" id="save">Update Info</button>');
+		this.$el.html(update_user_template());
 	},
 	save: function() {
 		//data before changes made
@@ -68,21 +77,52 @@ var UserView = Backbone.View.extend({
 			this.model.replace(homeVal);
 		}
 	},
-	initialize: function() {
-			this.model.on("change", this.render, this);
-	},
+	// initialize: function() {
+	// 	this.model.on("change", this.render, this);
+	// },
 	events: {
-			'click #update': "update",
-			'click #logout': "logout",
-			'click #save': "save"
-	}, //closes events
+		'click #update': "update",
+		'click #logout': "logout",
+		'click #save': "save"
+	} //closes events
 }); // closes userView
 
-var userModel, userView;
+//example
+// var Todo = Backbone.Model.extend({
+// 	defaults: {
+// 		completed: false
+// 	},
+// 	initialize: function(){
+// 		console.log('this todo model has been initialized');
+// 	}
+// });
+// var todo1 = new Todo();
+// console.log(JSON.stringify(todo1));
 
-$(document).ready(function(){
-	userModel = new UserModel();
-	userView = new UserView({model: userModel});
-	userView.render();
-	$("#userDiv").append(userView.$el);
-});
+// var TodoView = Backbone.View.extend({
+// 	tagName: 'li',
+// 	todoTpl: _.template("an example template"),
+// 	render: function(){
+// 		this.$el.html(this.todoTpl(this.model.attributes));
+// 		this.input = this.$('.edit');
+// 		return this;
+// 	}
+// });
+// var todoView = new TodoView();
+// console.log(todoView.el);
+
+var userModel1 = new UserModel();
+console.log(JSON.stringify(userModel1));
+
+var userView = new UserView({model: userModel1});
+console.log(userView.el);
+
+// var userModel, userView;
+
+// $(document).ready(function(){
+// 	console.log("tests");
+// 	userModel = new UserModel();
+// 	userView = new UserView({model: userModel});
+// 	userView.render();
+// 	$("#userDiv").append(userView.$el);
+// });
