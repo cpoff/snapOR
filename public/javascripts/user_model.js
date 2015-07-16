018b6ccad0d3e092cdf97be53645c3f134aa9fc0
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-16 15:19:53 -0700

    working on storing the registration data into the model

--- public/javascripts/user_model.js
+++ public/javascripts/user_model.js
@@ -1,4 +1,3 @@
-'use strict'
 _.templateSettings = {
 	interpolate: /\{\{(.+?)\}\}/g
 };
@@ -15,30 +14,30 @@ var UserModel = Backbone.Model.extend({
 		console.log('new model created');
 		this.fetch();
 	},
-	// replace: function(str) {
-	// 		this.set("name", str);
-	// 		this.set("email", str);
-	// 		this.set("home", str);
-	// 		this.save();
-	// }
+	replace: function(name, email, home) {
+			this.set({"name" : str, "email" : email, "home" : home});
+			// this.set("email", str);
+			// this.set("home", str);
+			this.save();
+	}
 }); // closes userModel
 
 var UserView = Backbone.View.extend({
 	url: '/user',
-	// new_user_template :  _.template('<h2>Welcome</h2><p>We have a couple more questions for you so we can make your experience with snapOR more personal.<p><label>Name: </label><input type="text" id="nameInput" placeholder="Who are you?" value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder="Where do you live?" value=""</input><br /><button type="submit" id="save">Save Info</button>'),
-	user_template : _.template('<h1>Welcome to snapOR! {{nameVal}}<button type="submit" id="update">Update info</button><button type="submit" id="logout">Logout</button>'),
 	update_user_template : _.template('<h2>Update</h2><label>Name: </label><input type="text" id="nameInput" placeholder={{nameVal}} value=""</input><br /><label>Email: </label><input type="text" id="emailInput" placeholder={{emailVal}} value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder={{homeVal}} value=""</input><br /><label>Password: </label><input type="text" id="password" placeholder="change password" value=""</input><br /><button type="submit" id="saveBtn">Update Info</button>'),
 	render: function() {
 		var nameVal = this.model.get("name");
 		var emailVal = this.model.get("email");
 		var homeVal = this.model.get("home");
 		var new_user_template =  _.template('<div id="userInfoDiv"><h2>Welcome, {{emailVal}}</h2><p>Please review your information below, and update as needed.<p><label>Name: </label><input type="text" id="nameInput" placeholder="Name" value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" placeholder="Where do you live?" value=""</input><br /><button type="submit" id="save">Save Info</button></div>');
-		if(nameVal === '' && homeVal === ''){
-			this.$el.html(new_user_template({emailVal : this.model.get("email")}));
-		} else{
-			this.$el.html(
-				user_template({nameVal : this.model.get("name")}));
-		}
+		var user_template = _.template('<h1>Welcome to snapOR! {{nameVal}}<button type="submit" id="update">Update info</button><button type="submit" id="logout">Logout</button>');
+		this.$el.html(new_user_template({emailVal : this.model.get("email")}));
+		// if(nameVal === '' && homeVal === ''){
+		// 	this.$el.html(new_user_template({emailVal : this.model.get("email")}));
+		// } else{
+		// 	this.$el.html(
+		// 		user_template({nameVal : this.model.get("name")}));
+		// }
 	}, // closes render again
 	update: function(){
 		var nameVal = this.model.get("name");
@@ -57,7 +56,10 @@ var UserView = Backbone.View.extend({
 		var homeChanged = this.$el.find("#homeInput").val();
 
 		if(nameVal !== nameChanged || emailVal !== emailChanged || omeVal !== homeChanged){
-			this.model.update();
+			// this.model.update();
+			this.model.set("name", nameChanged);
+			this.model.set("email", emailChanged);
+			this.model.set("home", homeChanged);
 		}
 		// if(emailVal !== emailChanged){
 		// 	this.model.update();
