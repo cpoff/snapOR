7eae5c46512a543ac05f1b24fe171ba70f127516
cpoff <curt.poff@gmail.com>
2015-07-23 11:42:15 -0700

    Park div now appearing correctly

--- public/javascripts/park_model.js
+++ public/javascripts/park_model.js
@@ -1,38 +1,37 @@
 _.templateSettings = {
-    interpolate: /\{\{(.+?)\}\}/g
+		interpolate: /\{\{(.+?)\}\}/g
 };
 
 // snapOR homepage
-var MasterView = Backbone.View.extend({
-    render: function() {
-        this.$el.html("<div>" + "Map API response goes here" + "</div>");
-    }
-});
+//var MasterView = Backbone.View.extend({
+//		render: function() {
+//				this.$el.html("<div>" + "Map API response goes here" + "</div>");
+//		}
+//});
 
 var ParkModel = Backbone.Model.extend({
-    // urlRoot: '/parkdetail',
-    urlRoot: '/',
-    defaults: {
-        'park_name': 'blah',
-        'park_latitude': '0',
-        'park_longitude': '0',
-        'parkFlickrCall': 'http://',
-    },
-    initialize: function() {
-        this.fetch();
-    }
+		// urlRoot: '/parkdetail',
+		urlRoot: '/',
+		defaults: {
+				'park_name': 'blah',
+				'park_latitude': '0',
+				'park_longitude': '0',
+				'parkFlickrCall': 'http://',
+		},
+		initialize: function() {
+				this.fetch();
+		}
 });
 
 var ParkView = Backbone.View.extend({
-    url: '/',
-    el: '#markerdiv',
-    render: function() {
-        var template = _.template('<h1>{{parkName}}</h1><div>{{FlickrInfo}}</div>');
-        this.$el.html(template({
-            parkName: 'dog',
-            FlickrInfo: 'flickr_data'
-        }));
-    },
+		url: '/',
+		render: function() {
+				var template = _.template('<h1>{{parkName}}</h1><div>{{FlickrInfo}}</div>');
+				this.$el.html(template({
+						parkName: 'park_name',
+						FlickrInfo: 'flickr_data'
+				}));
+		},
 });
 
 var MarkerView = Backbone.View.extend({
@@ -89,36 +88,37 @@ var markerArray = [];
 var theMap = {};
 
 var MapView = Backbone.View.extend({
-    el: '#map_canvas',
-    render: function() {
-        //creates map on the page
-        var mapCanvas = document.getElementById('map_canvas');
-        var Bend = new google.maps.LatLng(44.058173, -121.31531);
-        var mapOptions = {
-            center: Bend,
-            zoom: 7,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        theMap.map = new google.maps.Map(mapCanvas, mapOptions);
-        //builds marker views as map is generated
-        this.collection.each(function(park) {
-            var markerView = new MarkerView({
-                model: park,
-                map: theMap.map,
-            });
-            markerView.render();
-            //            markerView.map = map;
-            markerArray.push(markerView);
-        });
-    }
+		el: '#map_canvas',
+		render: function() {
+				//creates map on the page
+				var mapCanvas = document.getElementById('map_canvas');
+				var Bend = new google.maps.LatLng(44.058173, -121.31531);
+				var mapOptions = {
+						center: Bend,
+						zoom: 7,
+						mapTypeId: google.maps.MapTypeId.ROADMAP
+				};
+				theMap.map = new google.maps.Map(mapCanvas, mapOptions);
+				//builds marker views as map is generated
+				this.collection.each(function(park) {
+						var markerView = new MarkerView({
+								model: park,
+								map: theMap.map,
+						});
+						markerView.render();
+						//            markerView.map = map;
+						markerArray.push(markerView);
+				});
+		}
 });
 
 var ParkCollection = Backbone.Collection.extend({
-    model: ParkModel,
-    url: '/',
-    initialize: function() {
-        this.fetch();
-    }
+		model: ParkModel,
+		//  url : "/parkdetail", commented out until we create a route in index.js, which may be unnecessary to keep this as a spa
+		url: '/',
+		initialize: function() {
+				this.fetch();
+		}
 });
 
 
@@ -143,3 +143,4 @@ var mapView = new MapView({
 });
 
 $("#map_canvas").append(mapView.$el);
+
