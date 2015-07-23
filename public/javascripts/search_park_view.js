14d1e9661ba72b0aa00f22f579fbe94bebaf28cb
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-23 14:45:33 -0700

    explore button adds the flickr pictures to the page and closes the modal

--- public/javascripts/search_park_view.js
+++ public/javascripts/search_park_view.js
@@ -2,7 +2,7 @@ var SearchParkView = Backbone.View.extend({
 	url: '/',
 	el: '#parks',
 	render: function(){
-		var template = _.template('<h2 id="modalTitle">Explore Parks</h2><a class="close-reveal-modal aria-label="Close">&times;</a><div id="parkList"><input id="parkName" class="typeahead" type="text" name="Enter park name:" style="font-family: \'Robot Slab\'" placeholder="Ex: \'cape kiwanda\', \'silver falls state park\'"><br><input id="searchParks" class="submit button" class="submit" value="Explore"></div>');
+		var template = _.template('<h2 id="modalTitle">Explore Parks</h2><a class="close-reveal-modal aria-label="Close">&times;</a><div id="parkList"><input id="parkName" class="typeahead" type="text" name="Enter park name:" style="font-family: \'Robot Slab\'" placeholder="Ex: \'cape kiwanda\', \'silver falls state park\'"><br><a href="#" class="close-reveal-modal"><input id="searchParks" class="submit button" class="submit" value="Explore"></a></div>');
 		this.$el.html(template());
 	},
 
@@ -14,30 +14,15 @@ var SearchParkView = Backbone.View.extend({
 		}
 		var location = parkNames.indexOf(parkName); //returns the index position of the park the user is searching for
 		var flickrUrl = this.collection.models[location].attributes.parkFlickrCall; //sets that index position as the collection.modesls index position, and stores the parkFlicker url into a new variable
-		console.log(flickrUrl);
 		var sourceArray = [];
 		$.getJSON(flickrUrl) //fires the flickr url
 			.always(function(data) {
 				newJson = JSON.parse(data.responseText.slice(14, -1));
-				console.log(newJson);
-				// sourceArray.push(newJson);
-				// console.log(newJson.photos.photo.farm);
-				// if (data && data.items) {
 				for(var i = 0; i<newJson.photos.photo.length; ++i){
 					var source = "http://farm" + newJson.photos.photo[i].farm + ".static.flickr.com/" + newJson.photos.photo[i].server + "/" + newJson.photos.photo[i].id + "_" + newJson.photos.photo[i].secret + "_" + "t.jpg";
-					// console.log('source');
 					sourceArray.push(source);
 					$("<img class=flickrPhoto src=" + source + ">").appendTo('body');
-					// }
-					// $.each(newJson.photos.photo, function(item) {
-					// 	var source = "http://farm" + newJson.photos.photo.farm + ".static.flickr.com/" + newJson.photos.photo.server + "/" + newJson.photos.photo.id + "_" + newJson.photos.photo.secret + "_" + "t.jpg";
-					// 		// .appendTo("#parkdiv");
-					// });
 				}
-//                        console.log(newJson.photos.photo.length);
-			})
-			.always(function(){
-				console.log(sourceArray);
 			});
 	},
 	events: { 
