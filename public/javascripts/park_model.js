6062f3cabe3941ba910f29c3dcf5db74d63e3eed
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-23 13:14:37 -0700

    search park view is in separate file, all previous code from park model is removed, gulp appended for changes

--- public/javascripts/park_model.js
+++ public/javascripts/park_model.js
@@ -23,30 +23,6 @@ var ParkModel = Backbone.Model.extend({
 		}
 });
 
-var SearchParkView = Backbone.View.extend({
-	url: '/',
-	el: '#parks',
-	render: function(){
-		var template = _.template('<h2 id="modalTitle">Explore Parks</h2><a class="close-reveal-modal aria-label="Close">&times;</a><div id="parkList"><input id="parkName" class="typeahead" type="text" name="Enter park name:" style="font-family: \'Robot Slab\'" placeholder="Ex: \'cape kiwanda\', \'silver falls state park\'"><br><input id="searchParks" class="submit button" class="submit" value="Explore"></div>');
-		this.$el.html(template());
-	},
-
-	searchParks: function(){
-		var parkName = $("#parkName").val(); //store the user's input of a park they want to search for
-		var parkNames = [];
-		for(var i = 0; i<this.collection.models.length; i++){ //store all the park names into an array from the collection.model
-			parkNames.push(this.collection.models[i].attributes.name);
-		}
-		var location = parkNames.indexOf(parkName); //returns the index position of the park the user is searching for
-		var flickrUrl = this.collection.models[location].attributes.parkFlickrCall; //sets that index position as the collection.modesls index position, and stores the parkFlicker url into a new variable
-		$.getJSON(flickrUrl); //fires the flickr url
-
-	},
-	events: { 
-			'click #searchParks': 'searchParks'//fire flicker api, render parkView
-	}
-});
-
 var ParkView = Backbone.View.extend({
 		url: '/',
 		render: function() {
@@ -148,7 +124,3 @@ var mapView = new MapView({
 });
 
 $("#map_canvas").append(mapView.$el);
-
-var searchParkView = new SearchParkView({model: parkModel, collection: parkCollection});
-searchParkView.render();
-$("#parks").append(searchParkView.$el);
