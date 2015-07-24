e8b1dd8eb61a4fef4fc95ede4d28406bc4d178bc
[angela riggs] <riggs.ang@gmail.com>
2015-07-23 19:04:38 -0700

    went back to orig. code, no modal popping up

--- public/javascripts/user_model.js
+++ public/javascripts/user_model.js
@@ -25,38 +25,29 @@ var UserView = Backbone.View.extend({
 		var nameVal = this.model.get("name");
 		var emailVal = this.model.get("email");
 		var homeVal = this.model.get("home");
+
 		//NAV BAR OPTIONS FOR EXIS. USERS	
 		//BUTTON ID = UPDATE
-		// var user_template = _.template('<h2>Welcome {{nameVal}}</h2><div><button type="sumbit" id="update">Update</button><button type="sumbit" id="logout">Logout</button>');
-		// if(nameVal === '' && homeVal === ''){
-		// 	this.$el.html(new_user_template({emailVal : this.model.get("email")}));
-		// } else {
-		// 	this.$el.html(
-		// 		user_template({nameVal : this.model.get("name")}));
-		// }
-		var new_user_template =  _.template(
+		var current_user_template = _.template('<h2>Welcome {{nameVal}}</h2><div><button type="sumbit" id="update">Update</button><button type="sumbit" id="logout">Logout</button>');
+		if(nameVal === '' && homeVal === ''){
+			this.$el.html(new_user_template({emailVal : this.model.get("email")}));
+		} else {
+			this.$el.html(
+				current_user_template({nameVal : this.model.get("name")}));
+			}	
+		//MODAL WINDOW FOR NEW USER TO ENTER NAME/HOMETOWN
+		//BUTTON ID = COMPLETE_REGIS
+			var new_user_template =  _.template(
 				'<div id="userInfoDiv"><h2>Welcome, {{emailVal}}</h2><p>Please review your information below, and update as needed.</p><form method="post" action="/complete_regis"><label id="userLabel">Name:</label><input id="nameInput" type="text" placeholder="Name"</input><br /><label id="userLabel">Email: </label><input id="emailInput" type="text" value="{{emailVal}}"</input><br /><label id="userLabel">Home Location: </label><input id="homeInput" type="text" placeholder="Where do you live?"</input><br /><button id="complete_regis" type="submit">Save Info</button></form></div>');
-			console.log('welcome user');
-			// $('#regAccount').html(new_user_template({emailVal : this.model.get("email")}));
-		//},
+				console.log('welcome user')
 	}, // closes render
-	//MODAL WINDOW FOR NEW USER TO ENTER NAME/HOMETOWN
-	//BUTTON ID = COMPLETE_REGIS
-
-	//renderTemplate: function() {
 	events: {
-		'click #create_user': 'create_user',
-		'click .fire_new_modal': 'fire_new_modal',
+		'click #create_user': 'create_user',//new_user_template
 		'click #complete_regis': 'complete_regis',
-		'click #update': "update",//user_template
+		'click #update': "update",//current_user_template
 		'click .save': 'save',//update_user_template
 		'click #logout': 'logout',
 		'click #login_user': 'login_user' 
-		//need an event to render the error message
-	},
-	fire_new_modal: function() {
-		$("div#register.reveal-modal.users.open").replaceWith("#userInfoDiv");
-		$(".reveal-modal-bg").css({display: "none"});
 	},
 	login_user: function() {
 		var self = this;
@@ -70,15 +61,13 @@ var UserView = Backbone.View.extend({
 			}
 		});
 	},
-	//COMPLETE_REGIS RUNS WHEN USER CLICKS 'SAVE INFO'
+	//COMPLETE_REGIS RUNS WHEN USER CLICKS 'SAVE INFO' on new_user_template
 	complete_regis: function(){
 		var userName = $('#nameInput').val();
 		var userLocation = $('#homeInput').val();
 		var userEmail = $('#emailInput').val();
 
 		this.model.replace(userName, userEmail, userLocation);
-		// this.model.replace("home", userLocation);
-		// this.model.replace("email", userEmail);
 		userView.render();
 		$("#user").append(userView.$el.html());
 	},
@@ -88,32 +77,19 @@ var UserView = Backbone.View.extend({
 		var password = $('#password').val();
 		var password_confirm = $('#password_confirm').val();
 		if(password===password_confirm){
-			//jQuery.post( url [, data ] [, success ] [, dataType ] )
 			jQuery.post('/begin_regis', {email: userEmail, password: password});
-			// $("div#register.reveal-modal.users.open").replaceWith("#userInfoDiv");
-			// $(".reveal-modal-bg").css({display: "none"});
-				//function (reply) {
-				// if (reply.error) {
-				// 	console.log(reply);
-				// 	alert("Please use a unique email to register your account.");
-				// } else {
-				// 	console.log(self);
-				// 	self.complete_regis();
-				// }
-			//})
-			//this.model.set("email", userEmail);
-			//userView.render();
-			//$("#userDiv").append(userView.$el.html());
-		} else {
+			} else {
 			alert('Please make sure your password and password confirmation are the same.');
 		}
 	},
+	///////////////////// CURRENT_USER_TEMPLATE
 	update: function(){
 		var nameVal = this.model.get("name");
 		var emailVal = this.model.get("email");
 		var homeVal = this.model.get("home");
 		this.$el.html(update_user_template());
 	},
+	///////////////////// UPDATE_USER_TEMPLATE
 	save: function() {
 		//data before changes made
 		var nameVal = this.model.get("name");
