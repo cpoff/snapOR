0d68a59fd90e1ca018cd2ab25b0c5ecd74d3c2b3
[angela riggs] <riggs.ang@gmail.com>
2015-07-24 15:51:56 -0700

    removed reg-code.js

--- reg-code.js
+++
@@ -1,8 +0,0 @@
-//jade: click #create_user
-//bb: create_user function: pw - pwconfirm check
-//server: email check in orchestrate
-bb: second modal window for name/hometown
-bb: click #complete_regis
-server: db.put
-
-bb: click #save should also update to orchestrate (db.put)
