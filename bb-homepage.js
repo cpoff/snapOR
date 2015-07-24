61f185b94ef6ac235d84cf183989785e7b292ff1
[angela riggs] <riggs.ang@gmail.com>
2015-07-24 11:17:31 -0700

    new_user_temp is populating on page after you click 'register account' button

---
+++ bb-homepage.js
@@ -0,0 +1,10 @@
+new-user-template = _.template('<nav><ul><li id="site-title"><h1><a href="/">snapOR></a></h1></li><li id="explore"><a href="/">Explore Parks></a></li><li id="login"><a href="/">Login></a></li><li id="register"><a href="/">Register></a></li></ul></nav>');
+
+
+main view for whole mage
+map view
+register view
+lgin view
+nav view
+	would stay in main view
+	render function 
