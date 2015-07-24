073993cc72c842316ff102add2a5d6ef5ea8cdde
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-24 12:40:58 -0700

    park name get's replaced if multiple parks are searched

--- public/javascripts/search_park_view.js
+++ public/javascripts/search_park_view.js
@@ -19,12 +19,20 @@ var SearchParkView = Backbone.View.extend({
 		$.getJSON(flickrUrl) //fires the flickr url
 			.always(function(data) {
 				newJson = JSON.parse(data.responseText.slice(14, -1));
-				$("<h1 id='parkName'>"+name+"</h1>").appendTo('#parkHeader');
-				for(var i = 0; i<newJson.photos.photo.length; ++i){
-					var source = "http://farm" + newJson.photos.photo[i].farm + ".static.flickr.com/" + newJson.photos.photo[i].server + "/" + newJson.photos.photo[i].id + "_" + newJson.photos.photo[i].secret + "_" + "t.jpg";
-					var link = "http://www.flickr.com/photos/" + newJson.photos.photo[i].owner + "/" + newJson.photos.photo[i].id;					
-					sourceArray.push(source);
-					$("<a href=" + link + "><img class=flickrPhoto src=" + source + "></a>").appendTo('#pictures');
+				if($("#parkLabel").val()===undefined){
+					$("<h1 id='parkLabel'>"+name+"</h1>").appendTo('#parkHeader');
+				} else {
+					$("#parkHeader").replaceWith("<h1 id='parkLabel'>"+name+"</h1>");
+				}
+				if(newJson.photos.photo.length<1){
+					for(var i = 0; i<newJson.photos.photo.length; ++i){
+						var source = "http://farm" + newJson.photos.photo[i].farm + ".static.flickr.com/" + newJson.photos.photo[i].server + "/" + newJson.photos.photo[i].id + "_" + newJson.photos.photo[i].secret + "_" + "t.jpg";
+						var link = "http://www.flickr.com/photos/" + newJson.photos.photo[i].owner + "/" + newJson.photos.photo[i].id;					
+						sourceArray.push(source);
+						$("<a href=" + link + "><img class=flickrPhoto src=" + source + "></a>").appendTo('#pictures');
+					}
+				} else {
+					$("<p>Sorry, we couldn't find any photos from that park.</p>").appendTo('#pictures');
 				}
 			});
 	},
