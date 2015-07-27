821f70f548cf5ce2709e67fe59730decb5da0ddc
[angela riggs] <riggs.ang@gmail.com>
2015-07-27 09:04:00 -0700

    registration works again

--- routes/index.js
+++ routes/index.js
@@ -42,11 +42,12 @@ router.post('/begin_regis', function(req, res){
 	//console.log(db.search);
 	db.search('snap', 'value.email:'+email)
 	.then(function(result) {
-		console.log("search result");
-		console.log(result.body);
+		console.log(result);
 		//console.log('email: ', email)
 		if (result.body.count !== 0) {
-			res.send ({
+			console.log("search result");
+			console.log(result.body.count);
+			res.render ('mistake', {
 				error: "Email has already been used to register."});
 		} 
 		// else if(!validateEmail(email)){
@@ -55,32 +56,33 @@ router.post('/begin_regis', function(req, res){
 		// 		text: "Please click here to return to the home page: "});
 		// } 
 		else {
-				//The user's registration info
-				var raw = {email: 'email', password: 'password'};
-				//The info that gets stored
-				var stored = {email : 'email', salt:'', hash:''};
+			//The user's registration info
+			var raw = {email: email, password: password};
+			//The info that gets stored
+			var stored = {email : email, salt:'', hash:''};
 
-				function register(raw) {
-					//Create and store encrypted user record:
-					pwd.hash(raw.password, function(err,salt,hash) {
-						stored = {email:raw.email, salt:salt, hash:hash};
-						db.put('snap', user_key, {
-							'email': stored.email,
-							// 'password': password
-							'salt': stored.salt,
-							'hash': stored.hash
-						})// closes db.put
-						.then(function(){
-							console.log('user created');
-							console.log("db push");
-							//console.log(stored);
-							res.end();
+			var register = function (raw) {
+				//Create and store encrypted user record:
+				pwd.hash(raw.password, function(err,salt,hash) {
+					stored = {email:raw.email, salt:salt, hash:hash};
+					console.log(stored);
+					db.put('snap', user_key, {
+						'email': stored.email,
+						// 'password': password
+						'salt': stored.salt,
+						'hash': stored.hash
+					})// closes db.put
+					.then(function(){
+						console.log('user created');
+						console.log("db push");
+						console.log(stored);
+						res.end();
 						})// closes .then
-						.fail(function(err){});
-					});// closes pwd.hash
-				}// closes function register(raw)
+					.fail(function(err){});
+				});// closes pwd.hash
+			};// closes function register(raw)
 
-				register(raw);
+			register(raw);
 		}// closes else
 	});// closes initial db query for existing email
 });// closes router.post
@@ -132,7 +134,7 @@ router.post('/user', function(req, res) {
 
 		} else {
 			authenticate();
-			function authenticate(){
+			var authenticate = function(){
 				console.log(currentUser);
 				console.log("attempt to auth");
 				pwd.hash(password, currentUser.salt, function(err, hash){
@@ -140,7 +142,7 @@ router.post('/user', function(req, res) {
 					console.log(hash);
 					if(currentUser.hash===hash){
 						console.log("success");
-						res.redirect('/user')
+						res.redirect('/user');
 					} else {
 						res.render('mistake', {
 							error: "It looks like your password was incorrect.",
@@ -148,11 +150,44 @@ router.post('/user', function(req, res) {
 						});// closes response.render
 					}// closes else
 				});// closes pwd.hash
-			}// closes function authenticate
+			};// closes function authenticate
 		}// closes else
 	});// closes .then
 });// closes login router
 
+/* ROUTE FOR UPDATING EXISTING USER INFO */  
+router.post('/update_user_info', function(req, res) {
+	var email = req.body.email;
+	var password = req.body.password;
+	var name = req.body.name;
+	var hometown = req.body.hometown;
+	var database = app.get('database');
+
+	db.search('snap', 'value.email:'+email)
+	.then(function(result) {
+		var currentUser = result.body.results[0].value;
+		if (result.body.count === 0) {
+			console.log('no user to update');
+			res.render('mistake', {
+				error: 'We were not able to find your information',
+				text: 'Please try again.'
+			});// res.render
+		} else {
+			dp.put('snap', user_key, {
+				'email': stored.email,
+				'salt': stored.salt,
+				'hash': stored.hash,
+				'name': stored.name,
+				'hometown': stored.hometown
+			})// db.put
+			.then(function() {
+				console.log('user updated');
+				res.end();
+			})// then
+			.fail(function(err){});
+		}
+	});// then
+});// closes route to update exis. user info
 
 
 module.exports = router;
