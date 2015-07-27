40160b7aa71ffdad78dd73468cae5192ce0fe256
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-27 11:50:21 -0700

    fixed bug on parks with no pictures

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -32,30 +32,119 @@ var MarkerView = Backbone.View.extend({
 				$.getJSON(flickrURL)
 					.always(function(data) {
 						newJson = JSON.parse(data.responseText.slice(14, -1));
-						if ($("#parkLabel").val() === undefined) {
-							$("<h1 id='parkLabel'>" + name + "</h1>").appendTo('#parkHeader');
-						} else {
-							$("#parkHeader").replaceWith("<h1 id='parkLabel'>" + name + "</h1>");
-						}
-						if (newJson.photos.photo.length > 0) {
-							$(".flickrPhoto").remove();
-							$("<p>").remove();
-							for (var i = 0; i < newJson.photos.photo.length; ++i) {
-								var source = "http://farm" + newJson.photos.photo[i].farm + ".static.flickr.com/" + newJson.photos.photo[i].server + "/" + newJson.photos.photo[i].id + "_" + newJson.photos.photo[i].secret + "_" + "m.jpg";
-								var link = "http://www.flickr.com/photos/" + newJson.photos.photo[i].owner + "/" + newJson.photos.photo[i].id + " target=_blank";
-								sourceArray.push(source);
-								$("<a href=" + link + "><img class=flickrPhoto src=" + source + "></a>").appendTo('#pictures');
-							}
-						} else {
-							$("<p>Sorry, we couldn't find any photos from that park.</p>").replaceAll('#pictures');
-						}
-					});
 
+						//if user clicks on a park with pictures, the pictures are added to the page
+						//if the user then clicks on a park without any pictures, the error message appears
+						//if a user clicks on a park with no pictures after that, nothing changes
+						//if error on page, $("parkInfo p").html() gives error message, adding .length, gives number
+						//chandler state wayside has no pictures
+						// $("#parkInfo").remove();
+						if($("#parkLabel").html()===undefined){
+							$("<h1 id='parkLabel'>" + name + "</h1>").appendTo('#parkHeader');
+							if (newJson.photos.photo.length > 0){
+								for (var i = 0; i < newJson.photos.photo.length; ++i) {
+									var source = "http://farm" + newJson.photos.photo[i].farm + ".static.flickr.com/" + newJson.photos.photo[i].server + "/" + newJson.photos.photo[i].id + "_" + newJson.photos.photo[i].secret + "_" + "m.jpg";
+									var link = "http://www.flickr.com/photos/" + newJson.photos.photo[i].owner + "/" + newJson.photos.photo[i].id + " target=_blank";
+									sourceArray.push(source);
+									$("<a href=" + link + "><img class=flickrPhoto src=" + source + "></a>").appendTo('#pictures');
+								}
+							} else {
+								$("<p>Sorry, we couldn't find any photos from that park.</p>").appendTo('#pictures');	
+							}
+						} else{
+							$("#parkLabel").remove();
+							$(".flickrPhoto").remove();
+							$("#pictures p").remove();
+							console.log("dom already populated");
+							$("<h1 id='parkLabel'>" + name + "</h1>").appendTo('#parkHeader');
+							if (newJson.photos.photo.length > 0){
+								for (var i = 0; i < newJson.photos.photo.length; ++i) {
+									var source = "http://farm" + newJson.photos.photo[i].farm + ".static.flickr.com/" + newJson.photos.photo[i].server + "/" + newJson.photos.photo[i].id + "_" + newJson.photos.photo[i].secret + "_" + "m.jpg";
+									var link = "http://www.flickr.com/photos/" + newJson.photos.photo[i].owner + "/" + newJson.photos.photo[i].id + " target=_blank";
+									sourceArray.push(source);
+									$("<a href=" + link + "><img class=flickrPhoto src=" + source + "></a>").appendTo('#pictures');
+								}
+							} else {
+								$("<p>Sorry, we couldn't find any photos from that park.</p>").appendTo('#pictures');	
+							}
+						}
+						// if($("#parkInfo").html().length<0){
+						// 	$("<h1 id='parkLabel'>" + name + "</h1>").appendTo('#parkHeader');
+						// 	if (newJson.photos.photo.length > 0){
+						// 		for (var i = 0; i < newJson.photos.photo.length; ++i) {
+						// 			var source = "http://farm" + newJson.photos.photo[i].farm + ".static.flickr.com/" + newJson.photos.photo[i].server + "/" + newJson.photos.photo[i].id + "_" + newJson.photos.photo[i].secret + "_" + "m.jpg";
+						// 			var link = "http://www.flickr.com/photos/" + newJson.photos.photo[i].owner + "/" + newJson.photos.photo[i].id + " target=_blank";
+						// 			sourceArray.push(source);
+						// 			$("<a href=" + link + "><img class=flickrPhoto src=" + source + "></a>").appendTo('#pictures');
+						// 		}
+						// 	} else {
+						// 		$("<p>Sorry, we couldn't find any photos from that park.</p>").appendTo('#pictures');	
+						// 	}
+					// } else{
+					// 	$("#parkInfo").remove();
+					// 	$("<h1 id='parkLabel'>" + name + "</h1>").appendTo('#parkHeader');
+					// 	if (newJson.photos.photo.length > 0){
+					// 		for (var i = 0; i < newJson.photos.photo.length; ++i) {
+					// 			var source = "http://farm" + newJson.photos.photo[i].farm + ".static.flickr.com/" + newJson.photos.photo[i].server + "/" + newJson.photos.photo[i].id + "_" + newJson.photos.photo[i].secret + "_" + "m.jpg";
+					// 			var link = "http://www.flickr.com/photos/" + newJson.photos.photo[i].owner + "/" + newJson.photos.photo[i].id + " target=_blank";
+					// 			sourceArray.push(source);
+					// 			$("<a href=" + link + "><img class=flickrPhoto src=" + source + "></a>").appendTo('#pictures');
+					// 		}
+					// 	} else {
+					// 		$("<p>Sorry, we couldn't find any photos from that park.</p>").appendTo('#pictures');	
+					// 	}
+						// }
+					});
 			};
 		})(marker));
 
 	}
 });
+
+
+
+						// if ($("#parkLabel").html() === undefined) {
+						// 	$("<h1 id='parkLabel'>" + name + "</h1>").appendTo('#parkHeader');
+						// } else {
+						// 	$("#parkHeader").replaceWith("<h1 id='parkLabel'>" + name + "</h1>");
+						// }
+						// // if($("parkInfo p").html().length>0){
+						// // 	$("parkInfo p").remove();
+						// // }
+						// if (newJson.photos.photo.length > 0 && $("#parkInfo p").html()===undefined) {
+						// 	console.log(newJson.photos.photo.length);
+						// 	console.log($("#parkInfo p").html());
+						// 	$(".flickrPhoto").remove();
+						// 	// $("#parkInfo p").remove();
+						// 	for (var i = 0; i < newJson.photos.photo.length; ++i) {
+						// 		var source = "http://farm" + newJson.photos.photo[i].farm + ".static.flickr.com/" + newJson.photos.photo[i].server + "/" + newJson.photos.photo[i].id + "_" + newJson.photos.photo[i].secret + "_" + "m.jpg";
+						// 		var link = "http://www.flickr.com/photos/" + newJson.photos.photo[i].owner + "/" + newJson.photos.photo[i].id + " target=_blank";
+						// 		sourceArray.push(source);
+						// 		$("<a href=" + link + "><img class=flickrPhoto src=" + source + "></a>").appendTo('#pictures');
+						// 	}
+						// } else if (newJson.photos.photo.length === 0 && $("#parkInfo p").html()===undefined) {
+						// 	$("<p>Sorry, we couldn't find any photos from that park.</p>").replaceAll('#pictures');
+						// } else if (newJson.photos.photo.length > 0 && $("#parkInfo p").html()!==undefined){
+						// 	console.log(newJson.photos.photo.length);
+						// 	console.log($("#parkInfo p").html());
+						// 	// $(".flickrPhoto").remove();
+						// 	// debugger;
+						// 	$("#parkInfo p").remove();
+						// 	$("#parkHeader").replaceWith("<h1 id='parkLabel'>" + name + "</h1>");
+						// 	for (var i = 0; i < newJson.photos.photo.length; ++i) {
+						// 		var source = "http://farm" + newJson.photos.photo[i].farm + ".static.flickr.com/" + newJson.photos.photo[i].server + "/" + newJson.photos.photo[i].id + "_" + newJson.photos.photo[i].secret + "_" + "m.jpg";
+						// 		var link = "http://www.flickr.com/photos/" + newJson.photos.photo[i].owner + "/" + newJson.photos.photo[i].id + " target=_blank";
+						// 		sourceArray.push(source);
+						// 		$("<a href=" + link + "><img class=flickrPhoto src=" + source + "></a>").appendTo('#pictures');
+						// 	}
+// 						}
+// 					});
+
+// 			};
+// 		})(marker));
+
+// 	}
+// });
 
 var markerArray = [];
 var theMap = {};
