4d28aac6eb9f4009153dec3fd1e00e1d570cf4f3
[angela riggs] <riggs.ang@gmail.com>
2015-07-28 17:06:29 -0700

    login and register routes are now done through backbone, not the jade form. Initial try of toggling class works on login - the 'display-nav' class is removed and 'hide-nav' class is added

--- public/javascripts/user_model.js
+++ public/javascripts/user_model.js
@@ -14,25 +14,13 @@ var UserModel = Backbone.Model.extend({
 	}
 }); // closes userModel
 
-var UserView = Backbone.View.extend({
-	el : '#register',
-	// REGISTRATION PROCESS
-	render: function() {
-		var nameVal = this.model.get("name");
-		var emailVal = this.model.get("email");
-		var homeVal = this.model.get("home");
-	}, // closes render
+var RegView = Backbone.View.extend({
+	el: '#register',
 	events: {
-		'click #register_account': 'register_account',
-		'click #login_user': 'login_user', 
-		'click #update_user': 'update_user', // 'update my account' button from foundation modal
-		'click #logout': 'logout'
+		'click #register_account': 'register_account'
 	},
-	// logout: function() {
-	// 	jQuery.post('/logout');
-	// },
-	///////////////////// PROC. TO REGISTER NEW USER FROM LANDING PAGE
-	register_account: function(){
+	register_account: function(event){
+		event.preventDefault();
 		var self = this;
 		var userEmail = $('#email').val();
 		var password = $('#password').val();
@@ -49,14 +37,48 @@ var UserView = Backbone.View.extend({
 			alert('Please make sure your password and password confirmation are the same.');
 		}
 	},
-	login_user: function() {
-		var self = this;
+
+});// closes RegView
+
+var UserView = Backbone.View.extend({
+	el : '#login',
+	// REGISTRATION PROCESS
+	// render: function() {
+	// 	var nameVal = this.model.get("name");
+	// 	var emailVal = this.model.get("email");
+	// 	var homeVal = this.model.get("home");
+	// }, // closes render
+	events: {
+		//'click #register_account': 'register_account',
+		'click #login_user': 'login_user', 
+		'click #update_user': 'update_user',
+		'click #my_account': 'my_account',
+		'click #logout': 'logout',
+
+	},
+	// logout: function() {
+	// 	jQuery.post('/logout');
+	// 	remove cookie
+	// },
+	///////////////////// PROC. TO REGISTER NEW USER FROM LANDING PAGE
+	login_user: function(event) {
+		var userEmail = $('#login-email').val();
+		var password = $('#login-pw').val();
+		console.log('login user func')
+		event.preventDefault();
+		// var self = this;
+		//$('ul#logout-state').toggleClass('hide-nav');
 		jQuery.post('/user', {email: userEmail, password: password})
 		.then(function() {
 			console.log('new View')
-			$('div.logged-out').hide();
+			$('ul#logout-state').toggleClass('hide-nav display-nav', addOrRemove);
+			$('.reveal-modal-bg').css('display', 'none');
 		});
 	},
+	// my_account: (function() {
+	// 	var update_user_template = _.template('<h2>Update Account</h2><label>Name: </label><input type="text" id="nameInput" value={{nameVal}} </input><br /><label>Email: </label><input type="text" id="emailInput" value={{emailVal}} value=""</input><br /><label>Home Location: </label><input type="text" id="homeInput" value={{homeVal}}</input><br /><label>Password: </label><input type="text" id="password" placeholder="change password" value=""</input><br /><label>Password Confirm: </label><input type="text" id="password_confirm" placeholder="re-type password" value=""</input><br /><button type="submit" id="update_user">Update My Account</button>');
+	// 	$('#userInfoDiv').html(update_user_template({emailVal: userEmail, nameVal: userName, homeVal: userLocation}));
+	// })(),
 	update_user: function() {
 		//jQuery.post( url [, data ] [, success ] [, dataType ] )
 		jQuery.post('/update_user_info', {email: userEmail, password: password, name: nameInput, hometown: homeInput})
@@ -104,4 +126,13 @@ var UserView = Backbone.View.extend({
 
 var userModel = new UserModel();
 var userView = new UserView({model: userModel});
+var regview = new RegView({model: userModel});
+
+// $(function(){
+// 	$('#login-form').submit(function(e) {
+// 		e.preventDefault();
+// 		console.log("hi mom!");
+// 		$('#login_user').trigger('click')
+// 	})
+// });
 
